a81b7d8c5a860870e16171cb03687e77683be0ec
internetcafe@bk.ru <internetcafe@bk.ru>
2016-08-01 16:28:26 +0600

    Spring Session setup fixed.

--- src/main/java/loc/ecs/config/Initializer.java
+++ src/main/java/loc/ecs/config/Initializer.java
@@ -1,5 +1,6 @@
 package loc.ecs.config;
 
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
-public class Initializer implements WebApplicationInitializer {
+public class Initializer extends AbstractHttpSessionApplicationInitializer {
     @Override
     public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
